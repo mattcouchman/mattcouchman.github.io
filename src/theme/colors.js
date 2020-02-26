103b3b671074490764b37092371120f244901848
Matt Couchman <mattrusscouchman@gmail.com>
2020-02-26 20:31:25 +0000

    Updates

--- src/theme/colors.js
+++
@@ -1,7 +0,0 @@
-export default {
-  accent: "#d35400",
-  accentAlt: "#24252a",
-  white: "#ffffff",
-  whiteSemiTransparent: "rgba(255, 255, 255, 0.5)",
-  black: "#2e3131",
-}
--- src/theme/index.js
+++
@@ -1,9 +0,0 @@
-import colors from "./colors"
-import text from "./text"
-import heading from "./heading"
-
-export default {
-  colors,
-  text,
-  heading,
-}
