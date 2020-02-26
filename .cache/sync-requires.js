e467106292f27b542ebfd31d2d37f75c44163a55
Matt Couchman <mattrusscouchman@gmail.com>
2020-02-26 19:55:59 +0000

    gh-pages

--- .cache/sync-requires.js
+++ .cache/sync-requires.js
@@ -5,9 +5,8 @@ const preferDefault = m => m && m.default || m
 
 
 exports.components = {
-  "component---cache-dev-404-page-js": hot(preferDefault(require("/Users/mattcouchman/Projects/couch/.cache/dev-404-page.js"))),
-  "component---src-pages-404-js": hot(preferDefault(require("/Users/mattcouchman/Projects/couch/src/pages/404.js"))),
-  "component---src-pages-index-js": hot(preferDefault(require("/Users/mattcouchman/Projects/couch/src/pages/index.js"))),
-  "component---src-pages-page-2-js": hot(preferDefault(require("/Users/mattcouchman/Projects/couch/src/pages/page-2.js")))
+  "component---src-pages-404-js": hot(preferDefault(require("/Users/mattcouchman/Projects/mattcouchman.github.io/src/pages/404.js"))),
+  "component---src-pages-index-js": hot(preferDefault(require("/Users/mattcouchman/Projects/mattcouchman.github.io/src/pages/index.js"))),
+  "component---src-pages-page-2-js": hot(preferDefault(require("/Users/mattcouchman/Projects/mattcouchman.github.io/src/pages/page-2.js")))
 }
 
